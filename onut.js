1c73263d15b194ed9ba0ce8d4bb3840629052d98
Inanc <inancakduvann@gmail.com>
2021-01-02 23:32:22 +0300

    width option added

--- onut.js
+++ onut.js
@@ -1,4 +1,4 @@
-function Onut(id, data) {
+function Onut(id, data, config) {
     this.template = '<div class="onut-container"> \
                         <svg width="100%" height="100%" viewBox="0 0 40 40" class="onut" style="transform:rotate(270deg);"> \
                         <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle> \
@@ -8,10 +8,12 @@ function Onut(id, data) {
       
     this.colors =["#8ED081", "#B4D2BA", "#DCE2AA", "#B57F50", "#4B543B", "#e63946", "#a8dadc", "#457b9d", "#1d3557", "#fca311", "#e29578", "#560bad", "#fec89a", "#ffb5a7", "#f77f00",
         "#9b5de5", "#f15bb5", "#fee440", "#e26d5c", "#723d46", "#472d30", "#c9cba3", "#ffe1a8", "#9e2a2b", "#353535", "#344e41", "#e2afff", "#f3c4fb"
-    ]                
+    ]     
+    
+    this.width = config ? config.width ? config.width : 3.5 : 3.5;
                     
     this.createCircle = function(color, value, offset) {
-        return '<circle class="onut-slice" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke="' + color +'" stroke-width="3.5" stroke-dasharray="'+ value +' 100" stroke-dashoffset="'+ offset +'"></circle>'
+        return '<circle class="onut-slice" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke="' + color +'" stroke-width="'+ this.width +'" stroke-dasharray="'+ value +' 100" stroke-dashoffset="'+ offset +'"></circle>'
     }       
 
     this.generateNumber = function(min, max) {
@@ -62,5 +64,11 @@ new Onut("onut-demo", [
     },
     {
         value: 200
+    },
+    {
+        value: 120
     }
-]).draw();
+], 
+{
+    width: 3
+}).draw();
