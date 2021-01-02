e9a66c4dddd07a923ede05c29575ca97b055bc99
Inanc <inancakduvann@gmail.com>
2021-01-02 23:00:53 +0300

    values calculated as percentage

--- onut.js
+++ onut.js
@@ -1,6 +1,6 @@
 function Onut(id, data) {
     this.template = '<div class="onut-container"> \
-                        <svg width="100%" height="100%" viewBox="0 0 40 40" class="onut"> \
+                        <svg width="100%" height="100%" viewBox="0 0 40 40" class="onut" style="transform:rotate(270deg);"> \
                         <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle> \
                         </svg> \
                     </div>'    
@@ -11,12 +11,19 @@ function Onut(id, data) {
     
     this.draw = function() {
         var offset_val = 0;
+        var total = 0;
 
+        for(let i=0; i < data.length; i++) {
+            total = total +data[i].value;
+        }
+    
         document.getElementById(id).innerHTML = this.template;
 
         for(let i=0; i < data.length; i++) {
-            document.querySelector("#" + id + " .onut-container svg").innerHTML += this.createCircle(data[i].color, data[i].value, -offset_val);
-            offset_val = offset_val + data[i].value
+            var val = (data[i].value / total) * 100;
+
+            document.querySelector("#" + id + " .onut-container svg").innerHTML += this.createCircle(data[i].color, val, -offset_val);
+            offset_val = offset_val + val
         }
     }
 }
@@ -24,11 +31,15 @@ function Onut(id, data) {
 
 new Onut("onut-demo", [
     {
-        value: 25,
+        value: 124,
         color: "red"
     },
     {
-        value: 75,
+        value: 74,
         color: "salmon"
+    },
+    {
+        value: 214,
+        color: "green"
     }
 ]).draw();
